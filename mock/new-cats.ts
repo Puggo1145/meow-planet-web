fc92bd918fa48dd67e1ac0078b790927cd35a1e2
Andrew Zhao <puggoo1145@gmail.com>
2024-11-10 17:14:42 +0800

    fix: 🐛 fix interface type error

--- mock/new-cats.ts
+++ mock/new-cats.ts
@@ -1,21 +1,21 @@
-import type { INewCatsItemProps } from "@/app/(main)/today/_components/new-cats/new-cats-item";
-// mock assets
-import images from "@/constants/images";
+// import type { INewCatsItemProps } from "@/app/(main)/today/_components/new-cats/new-cats-item";
+// // mock assets
+// import images from "@/constants/images";
 
-export const mockNewCats: INewCatsItemProps[] = [
-    {
-        id: 1,
-        image: images.mockCat1,
-        name: "豆豆",
-    },
-    {
-        id: 2,
-        image: images.mockCat2,
-        name: "小黑",
-    },
-    {
-        id: 3,
-        image: images.mockCat3,
-        name: "小白",
-    }
-];
+// export const mockNewCats: INewCatsItemProps[] = [
+//     {
+//         id: 1,
+//         image: images.mockCat1,
+//         name: "豆豆",
+//     },
+//     {
+//         id: 2,
+//         image: images.mockCat2,
+//         name: "小黑",
+//     },
+//     {
+//         id: 3,
+//         image: images.mockCat3,
+//         name: "小白",
+//     }
+// ];
